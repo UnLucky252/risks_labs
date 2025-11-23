e88f28794085276b96ee64831b2ef51a8b3b2ddf
UnLucky252 <maksimanisimov03@gmail.com>
2025-11-23 11:04:11 +0300

    Add dirty hello.py implementation

---
+++ lab01/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# грязный код
+name=input("Введите имя: ")
+print("Hello appsec world from @"+name)
+
+# другой интерпретатор
+if True:
+    print("Это работает")
+        print("Неправильные отступы")
